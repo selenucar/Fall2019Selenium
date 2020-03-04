d59aa798d3395be41cbe4806bc0d020b10c8882f
selenucar <slnucartx@gmail.com>
2020-03-03 21:00:51 -0600

    utility

---
+++ src/test/java/com/automation/tests/day3/FindElementById.java
@@ -0,0 +1,62 @@
+package com.automation.tests.day3;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FindElementById {
+    public static void main(String[] args) throws Exception{
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://practice.cybertekschool.com/login");
+        driver.findElement(By.name("username")).sendKeys("tomsmith");
+        Thread.sleep(3000);
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("SuperSecretPassword");
+
+        driver.findElement(By.id("wooden_spoon")).click();
+        Thread.sleep(3000);
+        String expected = "Welcome to the Secure Area. When you are done click logout below.";
+        String actual = driver.findElement(By.tagName("h4")).getText();
+
+        if (expected.equals(actual)){
+            System.out.println("TEST PASSED");
+        }else {
+            System.out.println("TEST FAILED");
+        }
+
+        // let's click on logout button. it looks like a button, but it's actually a link
+        // every element with <a> is a link
+        // if you have couple space in the text, just use partialLinkText instead of linkText
+        // linkText - equals()
+        // partialLinkText - contains() - complete match doesn't required
+        // don't put space
+
+        WebElement logout = driver.findElement(By.linkText("Logout"));
+
+        String href = logout.getAttribute("href");
+        String className = logout.getAttribute("class");
+        System.out.println(href);
+        System.out.println(className);
+        logout.click();
+        Thread.sleep(3000);
+
+        // let's enter invalid credentials
+
+        driver.findElement(By.name("username")).sendKeys("wrong");
+        driver.findElement(By.name("password")).sendKeys("wrong");
+        driver.findElement(By.id("wooden_spoon")).click();
+
+        Thread.sleep(2000);
+
+        WebElement errorMessage = driver.findElement(By.id("flash-messages"));
+        System.out.println(errorMessage.getText());
+
+        Thread.sleep(2000);
+        driver.quit();
+
+    }
+}
--- src/test/java/com/automation/tests/day3/FindElementsPractice.java
+++ src/test/java/com/automation/tests/day3/FindElementsPractice.java
@@ -1,13 +1,53 @@
 package com.automation.tests.day3;
 
+import com.automation.utilities.DriverFactory;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FindElementsPractice {
-    public static void main(String[] args) {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+    public static void main(String[] args) throws Exception {
+//       WebDriverManager.chromedriver().setup();
+//        WebDriver driver = new ChromeDriver();
+
+        WebDriver driver = DriverFactory.createDriver("chrome");
+        driver.get("http://practice.cybertekschool.com/sign_up");
+        WebElement fullName = driver.findElement(By.name("full_name"));
+        fullName.sendKeys("Mister Twister");
+
+        Thread.sleep(3000);
+
+        WebElement email = driver.findElement(By.name("email"));
+        email.sendKeys("sdet@cynertek.com");
+
+        Thread.sleep(3000);
+
+        WebElement signUp = driver.findElement(By.name("wooden_spoon"));
+        // when you see type = "submit", you can use submit() instead of click()
+        // it make sense to use when click() method doesn't work
+        signUp.submit();
+        Thread.sleep(3000);
+
+        String expected = "Thank you for signing up. Click the button below to return to the home page.";
+
+        WebElement message = driver.findElement(By.className("subheader"));
+
+        String actual = message.getText(); // to get the text <h3>Text</h3>
+
+        if (expected.equals(actual)){
+            System.out.println("TEST PASSED");
+        }else {
+            System.out.println("TEST FAILED");
+        }
+
+
+        driver.quit();//to close everything
+
+
+
+
 
 
     }
